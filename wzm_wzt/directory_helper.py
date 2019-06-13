20515c1114a17267cdcccc3ed9f5196db83b1a53
Jennifer M. Hays <jmh5sf@virginia.edu>
2019-06-13 12:25:56 -0400

    update directory structure and tests to accomodate the new workflow

--- wzm_wzt/directory_helper.py
+++ wzm_wzt/directory_helper.py
@@ -1,6 +1,5 @@
-"""
-Organizes the directory structure for BRER runs.
-Creates directories on the fly.
+"""Organizes the directory structure for BRER runs. Creates directories on the
+fly.
 
 How the directory structure is organized:
     - This script should be run from your "top" directory (where
@@ -8,29 +7,41 @@ How the directory structure is organized:
     - The top directory contains ensemble member subdirectories
       This script is intended to handle just ONE ensemble member,
       so we'll only be concerned with a single member subdirectory.
-    - The example below is for the first iteration (iter 0) of one
-      of the members. Future iterations would go in directories
-      1,2,...y
-.
-├── 0
-│   ├── converge_dist
-│   │   ├── state.cpt
-│   │   ├── state_prev.cpt
-│   │   └── traj_comp.part0001.xtc
-│   ├── production
-│   │   ├── confout.part0005.gro
-│   │   ├── state.cpt
-│   │   ├── state_prev.cpt
-│   │   ├── state_step4622560.cpt
-│   │   ├── traj_comp.part0002.xtc
-│   └── training_alpha
-│       ├── state.cpt
-│       ├── state_prev.cpt
-│       └── traj_comp.part0001.xtc
-├── state.json
-├── submit.slurm
-└── syx.tpr
+    - The example below shows the directory structure at initialization.
 
+.
+└── 0                               # FIRST BRER ITERATION
+    ├── num_test_sites_1
+    │   └── production
+    ├── num_test_sites_2
+    │   └── production
+    ├── num_test_sites_3
+    │   └── production
+    ├── num_test_sites_4
+    │   └── production
+    ├── num_test_sites_5
+    │   └── production
+    └── num_test_sites_6            # At the first, we test all six different pairs with single target
+        ├── 10088_12035             # FIRST PAIR
+        │   ├── convergence         # only run training and convergence to get work calc'ns
+        │   └── training
+        ├── 3673_10088
+        │   ├── convergence
+        │   └── training
+        ├── 3673_12035
+        │   ├── convergence
+        │   └── training
+        ├── 3673_5636
+        │   ├── convergence
+        │   └── training
+        ├── 5636_10088
+        │   ├── convergence
+        │   └── training
+        ├── 5636_12035
+        │   ├── convergence
+        │   └── training
+        ├── production              # run a *SINGLE* production phase once you decide which pair to restrain.
+        └── work.json               # store work calculation data
 """
 
 import os
@@ -38,7 +49,8 @@ import os
 
 class DirectoryHelper:
     def __init__(self, top_dir, param_dict):
-        """Small class for manipulating a standard directory structure for BRER runs.
+        """Small class for manipulating a standard directory structure for BRER
+        runs.
 
         Parameters
         ----------
@@ -50,66 +62,59 @@ class DirectoryHelper:
 
         Returns
         -------
-
         """
         self._top_dir = top_dir
-        self._required_parameters = ['ensemble_num', 'iteration', 'phase']
+        self._required_parameters = ['ensemble_num', 'iteration', 'num_test_sites', 'test_site', 'phase']
         for required in self._required_parameters:
             if required not in param_dict:
                 raise ValueError('Must define {}'.format(required))
         self._param_dict = param_dict
 
     def get_dir(self, level):
-        """Get the directory for however far you want to go down the directory tree
+        """Get the directory for however far you want to go down the directory
+        tree.
 
         Parameters
         ----------
         level :
-            one of 'top', 'ensemble_num', 'iteration', or 'phase'.
+            one of 'top', 'ensemble_num', 'iteration', 'test_site', or 'phase'.
             See the directory structure example provided at the beginning of this class.
 
         Returns
         -------
         type
             the path to the specified directory 'level' as a str.
-
         """
         pdict = self._param_dict
         if level == 'top':
             return_dir = self._top_dir
         elif level == 'ensemble_num':
-            return_dir = '{}/mem_{}'.format(self._top_dir,
-                                            pdict['ensemble_num'])
+            return_dir = '{}/mem_{}'.format(self._top_dir, pdict['ensemble_num'])
         elif level == 'iteration':
-            return_dir = '{}/mem_{}/{}'.format(
-                self._top_dir, pdict['ensemble_num'], pdict['iteration'])
+            return_dir = '{}/mem_{}/{}'.format(self._top_dir, pdict['ensemble_num'], pdict['iteration'])
+        elif level == 'test_site':
+            return_dir = '{}/mem_{}/{}/num_test_sites_{}/{}'.format(self._top_dir, pdict['ensemble_num'],
+                                                                    pdict['iteration'], pdict['num_test_sites'],
+                                                                    pdict['test_site'])
         elif level == 'phase':
-            return_dir = '{}/mem_{}/{}/{}'.format(
-                self._top_dir, pdict['ensemble_num'], pdict['iteration'],
-                pdict['phase'])
+            return_dir = '{}/mem_{}/{}/num_test_sites_{}/{}/{}'.format(self._top_dir, pdict['ensemble_num'],
+                                                                       pdict['iteration'], pdict['num_test_sites'],
+                                                                       pdict['test_site'], pdict['phase'])
         else:
-            raise ValueError(
-                '{} is not a valid directory type for BRER simulations'.format(
-                    'type'))
+            raise ValueError('{} is not a valid directory type for BRER simulations'.format('type'))
         return return_dir
 
     def build_working_dir(self):
-        """Checks to see if the working directory for current state of BRER simulation exists.
-        If it does not, creates the directory.
+        """Checks to see if the working directory for current state of BRER
+        simulation exists. If it does not, creates the directory.
 
         Parameters
         ----------
 
         Returns
         -------
-
         """
-        if not os.path.exists(self.get_dir('phase')):
-            tree = [self.get_dir('ensemble_num'), self.get_dir('iteration')]
-            for leaf in tree:
-                if not os.path.exists(leaf):
-                    os.mkdir(leaf)
-            os.mkdir(self.get_dir('phase'))
+        os.makedirs(self.get_dir('phase'), exist_ok=True)
 
     def change_dir(self, level):
         """
--- wzm_wzt/tests/test_dir_helper.py
+++ wzm_wzt/tests/test_dir_helper.py
@@ -14,9 +14,17 @@ def test_directory(tmpdir):
     """
     my_home = os.path.abspath(os.getcwd())
     top_dir = tmpdir.mkdir("top_directory")
-    dir_helper = DirectoryHelper(top_dir, {'ensemble_num': 1, 'iteration': 0, 'phase': 'training'})
+    param_dict = {
+        'ensemble_num': 1,
+        'iteration': 0,
+        'num_test_sites': 6,
+        'test_site': '3673_5636',
+        'phase': 'training'
+    }
+
+    dir_helper = DirectoryHelper(top_dir, param_dict)
     dir_helper.build_working_dir()
     dir_helper.change_dir('phase')
-    assert (os.getcwd() == '{}/mem_{}/{}/{}'.format(top_dir, 1, 0, 'training'))
+    assert (os.getcwd() == '{}/mem_{}/{}/num_test_sites_{}/{}/{}'.format(top_dir, 1, 0, 6, '3673_5636', 'training'))
 
     os.chdir(my_home)
