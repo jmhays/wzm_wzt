b8ca283a2653262accb31fbc2e8d53c6bc7fcf73
Jennifer M. Hays <jmh5sf@virginia.edu>
2019-06-21 12:01:59 -0400

    a few travis updates

---
+++ devtools/travis-ci/install_gmx_master.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ev
+
+export GMX_DOUBLE=OFF
+export GMX_MPI=OFF
+export GMX_THREAD_MPI=ON
+export GMXAPI=ON
+
+pushd $HOME
+[ -d gromacs-gmxapi ] || git clone --depth=1 --no-single-branch https://github.com/kassonlab/gromacs-gmxapi.git
+pushd gromacs-gmxapi
+rm -rf build  # get rid of any possible lingering build directories
+mkdir build
+pushd build
+cmake ../ -DCMAKE_CXX_COMPILER=$CXX \
+    -DCMAKE_C_COMPILER=$CC \
+    -DGMX_DOUBLE=$GMX_DOUBLE \
+    -DGMX_MPI=$GMX_MPI \
+    -DGMX_THREAD_MPI=$GMX_THREAD_MPI \
+    -DGMXAPI=$GMXAPI \
+    -DCMAKE_INSTALL_PREFIX=$HOME/install/gromacs-gmxapi
+make -j2 install
+source $HOME/install/gromacs-gmxapi/bin/GMXRC
+popd
+popd
+popd
